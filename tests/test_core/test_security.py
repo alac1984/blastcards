c6466ae1dde7e98e8c09ef72216df01d01faffca
André Carvalho <alac1984@gmail.com>
2022-11-18 22:24:56 -0300

    100% coverage on core.security

---
+++ tests/test_core/test_security.py
@@ -0,0 +1,33 @@
+import pytest
+from jose import jwt
+from datetime import timedelta
+
+from core.security import create_access_token
+from core.config import settings
+
+
+@pytest.mark.unit
+def test_create_access_token_without_expiredelta():
+    data = {"sub": "user@test.com"}
+    jwt_token = create_access_token(data)
+    decoded = jwt.decode(
+        jwt_token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+    )
+
+    assert isinstance(jwt_token, str)
+    assert decoded["sub"] == "user@test.com"
+    assert "exp" in decoded
+
+
+@pytest.mark.unit
+def test_create_access_token_with_expiredelta():
+    data = {"sub": "user@test.com"}
+    expire_delta = timedelta(seconds=20)
+    jwt_token = create_access_token(data, expire_delta)
+    decoded = jwt.decode(
+        jwt_token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+    )
+
+    assert isinstance(jwt_token, str)
+    assert decoded["sub"] == "user@test.com"
+    assert "exp" in decoded
